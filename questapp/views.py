f7d483ef60fd6b165c07d2097755c72ae6a93653
phaggi <phaggi@gmail.com>
2021-12-31 00:31:16 +0300

    add views for vote, detail and results

--- questapp/views.py
+++ questapp/views.py
@@ -1,8 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
 
 # Create your views here.
-class MainView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'questapp/index.html')
+def index(request):
+    return HttpResponse("You're at the questapp index.")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at the question {question_id}.")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
+
+
