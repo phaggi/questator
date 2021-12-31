017fef337b81c6ebc90ccd07f2e914447b991860
phaggi <phaggi@gmail.com>
2021-12-31 23:45:09 +0300

    correct imprinted name

--- questapp/models.py
+++ questapp/models.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date_bublished')
+    pub_date = models.DateTimeField('date_published')
 
     def __str__(self):
         return self.question_text
