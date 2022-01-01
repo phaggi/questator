629bc6139f9fb709829fa00302b0381def40acb8
phaggi <phaggi@gmail.com>
2022-01-01 22:43:50 +0300

    add app_name to questapp.urls.py

--- questapp/urls.py
+++ questapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'questapp'
 urlpatterns = [path('', views.index, name='index'),
                path('<int:question_id>/', views.detail, name='detail'),
                path('<int:question_id>/results/', views.results, name='results'),
