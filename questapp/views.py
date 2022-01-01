b86e078baf47f8171139d97a64bab35ca1a568da
phaggi <phaggi@gmail.com>
2022-01-01 23:34:13 +0300

    add controller for results

--- questapp/views.py
+++ questapp/views.py
@@ -25,7 +25,9 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request=request, template_name='questapp/results.html', context=context)
 
 
 def vote(request, question_id):
