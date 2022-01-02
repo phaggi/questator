cd0aab5c3020bd6b7cd602740ee58dcee5b8ba11
phaggi <phaggi@gmail.com>
2022-01-03 01:16:28 +0300

    refactor views to generics

--- questapp/views.py
+++ questapp/views.py
@@ -1,34 +1,27 @@
-from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.views import View
 from .models import Question, Choice
-from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('questapp/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'questapp/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'questapp/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'questapp/detail.html'
 
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoNotExist:
-        raise Http404('Question does not exist')
-    context = {'question': question}
-    return render(request=request, template_name='questapp/detail.html', context=context)
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
-    return render(request=request, template_name='questapp/results.html', context=context)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'questapp/results.html'
 
 
 def vote(request, question_id):
