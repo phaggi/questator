cd55e0fa4eee687378bc69daf6d20159d2324d01
phaggi <phaggi@gmail.com>
2022-01-04 23:33:10 +0300

    add test for past questions in DetailView

--- questapp/tests.py
+++ questapp/tests.py
@@ -89,3 +89,9 @@ class QuestionDetailViewTests(TestCase):
         url = reverse(viewname='questapp:detail', args=(future_question.id,))
         responce = self.client.get(url)
         self.assertEqual(responce.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question(question_text='Past question.', days=-5)
+        url = reverse(viewname='questapp:detail', args=(past_question.id,))
+        responce = self.client.get(url)
+        self.assertContains(responce, past_question.question_text)
